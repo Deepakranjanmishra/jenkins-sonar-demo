6242eaeb8867e90d8a9f9c614e6db47340943e77
Deepakranjanmishra <126373117+Deepakranjanmishra@users.noreply.github.com>
2024-04-05 19:57:40 +0530

    Update CalculatorApplication.java

--- src/main/java/com/vcjain/calculator/CalculatorApplication.java
+++ src/main/java/com/vcjain/calculator/CalculatorApplication.java
@@ -8,10 +8,7 @@ public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
-		try{
-			int i = 0;
-		}catch(Exception e){
-		}
+		
 
 	}
 
